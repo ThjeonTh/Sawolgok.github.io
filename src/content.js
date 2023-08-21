075dd7d7baa7caed368c0c1234a982e2cbdae9be
ThjeonTh <115444184+ThjeonTh@users.noreply.github.com>
2023-08-22 03:47:23 +0900

    Update content.js

--- src/content.js
+++ src/content.js
@@ -1,1 +1,26 @@
 
+var mpFileName="";
+var midFileName="";
+var fileUrl="docs.google.com/uc?export=download&id=%FileName%&confirm=t";
+var fileType="";
+function Load(){
+    console.log(window.location.href);
+    const url = new URL(window.location.href);
+    const urlParams = url.searchParams;
+	mpFileName=urlParams.get('FileName');
+	midFileName=urlParams.get('midFileName');
+	console.log(mpFileName);
+	console.log(midFileName);
+    console.log(fileUrl);
+	var _url="https://drive.google.com/uc?id="+mpFileName;
+	document.getElementById("imageData").src=_url;
+	if(midFileName=="%midFileName%"){
+		console.log("m_midi hide");
+		document.getElementById("m_midi").style.display = "none";
+	}	
+}
+
+function OnImageDownload(){
+	var t_u="https://"+fileUrl.replace("%FileName%",mpFileName);
+	location.href=t_u;
+}
