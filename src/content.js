662a87d0baf4b21a1bfe27706b1d878c492861f6
ThjeonTh <115444184+ThjeonTh@users.noreply.github.com>
2024-03-22 06:56:30 +0900

    Update content.js
    오픈 수정

--- src/content.js
+++ src/content.js
@@ -9,15 +9,16 @@ function Load(){
     const urlParams = url.searchParams;
 	mpFileName=urlParams.get('FileName');
 	midFileName=urlParams.get('midFileName');
-	console.log(mpFileName);
-	console.log(midFileName);
-    console.log(fileUrl);
+	//console.log(mpFileName);
+	//console.log(midFileName);
+    	//console.log(fileUrl);
 	var _url="https://drive.google.com/uc?id="+mpFileName;
-	document.getElementById("imageData").src=_url;
+	//document.getElementById("imageData").src=_url;
 	if(midFileName=="%midFileName%"){
 		console.log("m_midi hide");
 		document.getElementById("m_midi").style.display = "none";
 	}	
+	window.open("https://drive.google.com/file/d/"+mpFileName+"/view");
 }
 
 function OnImageDownload(){
